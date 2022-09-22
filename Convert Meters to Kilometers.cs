7c4218accd0bcb4a36a050d207c369de4b24f9ed
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-22 20:33:15 +0300

    Add files via upload

---
+++ Convert Meters to Kilometers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1._Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distanceInMeterss = int.Parse(Console.ReadLine());
+            float distanceInKm = distanceInMeterss / 1000f;
+            Console.WriteLine($"{distanceInKm:f2}");
+        }
+    }
+}
