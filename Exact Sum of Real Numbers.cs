47b457f5fecb36329fd973c848d359aa36b8d7c4
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-22 21:47:24 +0300

    Add files via upload

---
+++ Exact Sum of Real Numbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Exact_Sum_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            decimal number;
+            decimal sumOfNumber = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                number = decimal.Parse(Console.ReadLine());
+                sumOfNumber += number;
+            }
+            Console.WriteLine(sumOfNumber);
+        }
+    }
+}
