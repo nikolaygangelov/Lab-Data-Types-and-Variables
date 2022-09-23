3e11e4e551199a63455d4b6e1b6f16f1b7e586de
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-23 23:31:30 +0300

    Add files via upload

---
+++ Town Info.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace _08._Town_Info
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            double area = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {town} has population of {population} and area {area} square km.");
+        }
+    }
+}
