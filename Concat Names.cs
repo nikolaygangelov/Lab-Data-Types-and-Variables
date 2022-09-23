55bf7b44ef5a04ed7b3e13a3d60d253c5b050667
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-23 23:30:36 +0300

    Add files via upload

---
+++ Concat Names.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace _07._Concat_Names
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = Console.ReadLine();
+            string name2 = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            Console.WriteLine($"{name1}{delimiter}{name2}");
+        }
+    }
+}
