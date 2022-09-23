89022a306b4845e0e7d6e38fca5a78c7f0e1d2c6
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-23 23:08:01 +0300

    Add files via upload

---
+++ Reversed Chars.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace _06._Reversed_Chars
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char character1 = char.Parse(Console.ReadLine());
+            char character2 = char.Parse(Console.ReadLine());
+            char character3 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{character3} {character2} {character1}");
+            
+
+
+        }
+    }
+}
