e25d533a743b28f627252b5782322216474533f5
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-24 18:41:03 +0300

    Add files via upload

---
+++ Refactor Volume of Pyramid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _11._Refactor_Volume_of_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            
+            double volume = (length * width * height)/3;
+            Console.WriteLine($"Length: {length}Width: {width}Height: {height}Pyramid Volume: {volume:f2}");
+
+        }
+    }
+}
