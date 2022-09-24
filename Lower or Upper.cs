098077f9a2c9a5ad02bf1d7ae7e2d00dffb055ac
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-24 17:57:03 +0300

    Add files via upload

---
+++ Lower or Upper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _10._Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string character = Console.ReadLine();
+
+            string upper = character.ToUpper();
+            if (upper == character)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
